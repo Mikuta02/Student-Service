1461613001dcd5b3611ad0758b333dbd4a629ed2
Mikuta02 <jakovljevicm002@gmail.com>
2023-11-08 16:01:31 +0100

    Uradjene klase Adresa,Indeks,Katedra,Ocena na ispitu

--- CLI/Model/Adresa.cs
+++ CLI/Model/Adresa.cs
@@ -8,5 +8,28 @@ namespace CLI.Model
 {
     internal class Adresa
     {
+
+        public string Ulica {  get; set; }
+        public int Broj { get; set; }
+        public string Grad {  get; set; }
+        public string Drzava { get; set; }
+
+        public Adresa(string ulica, int broj, string grad, string drzava)
+        {
+            Ulica = ulica;
+            Broj = broj;
+            Grad = grad;
+            Drzava = drzava;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("ULICA: {Ulica}, ");
+            sb.Append("BROJ: {Broj}, ");
+            sb.Append("GRAD: {Grad}, ");
+            sb.Append("DRZAVA: {Drzava}");
+            return base.ToString();
+        }
     }
 }
